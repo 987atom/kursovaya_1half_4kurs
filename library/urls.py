36e1adcd7224e7e4996bb257de746308d41f6777
987atom <isip_m.k.davydov@mpt.ru>
2024-12-16 01:51:15 +0300

    6

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ShelfListView, ShelfCreateView, ShelfUpdateView, ShelfDeleteView, guest_view, book_list, \
-    BookListView, BookCreateView, BookUpdateView, BookDeleteView, AuthorListView, AuthorCreateView, AuthorUpdateView, \
-    AuthorDeleteView, GenreListView, GenreCreateView, GenreUpdateView, GenreDeleteView, HomeView, librarian_login
+# from .views import ShelfListView, ShelfCreateView, ShelfUpdateView, ShelfDeleteView, guest_view, book_list, \
+#     BookListView, BookCreateView, BookUpdateView, BookDeleteView, AuthorListView, AuthorCreateView, AuthorUpdateView, \
+#     AuthorDeleteView, GenreListView, GenreCreateView, GenreUpdateView, GenreDeleteView, HomeView, librarian_login
 # from .views import register, login_view
-from .views import register, user_login
+# from .views import register, user_login
+from .views import *
 
 
 urlpatterns = [
@@ -13,25 +14,25 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('liblogin/', librarian_login, name='login_lib'),
 
-    path('shelves/', ShelfListView.as_view(), name='shelf_list'),  # Список полок
-    path('shelves/add/', ShelfCreateView.as_view(), name='shelf_add'),  # Добавление полки
-    path('shelves/edit/<int:pk>/', ShelfUpdateView.as_view(), name='shelf_edit'),  # Редактирование полки
-    path('shelves/delete/<int:pk>/', ShelfDeleteView.as_view(), name='shelf_delete'),  # Удаление полки
+    path('shelves/', ShelfListView.as_view(), name='shelf_list'),
+    path('shelves/add/', ShelfCreateView.as_view(), name='shelf_add'),
+    path('shelves/edit/<int:pk>/', ShelfUpdateView.as_view(), name='shelf_edit'),
+    path('shelves/delete/<int:pk>/', ShelfDeleteView.as_view(), name='shelf_delete'),
 
-    path('books_1/', BookListView.as_view(), name='book_list_1'),  # Список книг
-    path('books/add/', BookCreateView.as_view(), name='book_add'),  # Добавление книги
-    path('books/edit/<int:pk>/', BookUpdateView.as_view(), name='book_edit'),  # Редактирование книги
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),  # Удаление книги
+    path('books_1/', BookListView.as_view(), name='book_list_1'),
+    path('books/add/', BookCreateView.as_view(), name='book_add'),
+    path('books/edit/<int:pk>/', BookUpdateView.as_view(), name='book_edit'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
 
-    path('authors/', AuthorListView.as_view(), name='author_list'),  # Список авторов
-    path('authors/add/', AuthorCreateView.as_view(), name='author_add'),  # Добавление автора
-    path('authors/edit/<int:pk>/', AuthorUpdateView.as_view(), name='author_edit'),  # Редактирование автора
-    path('authors/delete/<int:pk>/', AuthorDeleteView.as_view(), name='author_delete'),  # Удаление автора
+    path('authors/', AuthorListView.as_view(), name='author_list'),
+    path('authors/add/', AuthorCreateView.as_view(), name='author_add'),
+    path('authors/edit/<int:pk>/', AuthorUpdateView.as_view(), name='author_edit'),
+    path('authors/delete/<int:pk>/', AuthorDeleteView.as_view(), name='author_delete'),
 
-    path('genres/', GenreListView.as_view(), name='genre_list'),  # Список жанров
-    path('genres/add/', GenreCreateView.as_view(), name='genre_add'),  # Добавление жанра
-    path('genres/edit/<int:pk>/', GenreUpdateView.as_view(), name='genre_edit'),  # Редактирование жанра
-    path('genres/delete/<int:pk>/', GenreDeleteView.as_view(), name='genre_delete'),  # Удаление жанра
+    path('genres/', GenreListView.as_view(), name='genre_list'),
+    path('genres/add/', GenreCreateView.as_view(), name='genre_add'),
+    path('genres/edit/<int:pk>/', GenreUpdateView.as_view(), name='genre_edit'),
+    path('genres/delete/<int:pk>/', GenreDeleteView.as_view(), name='genre_delete'),
 
     # path('admin/', admin_view, name='admin'),
     # path('librarian/', librarian_view, name='librarian'),
@@ -41,5 +42,5 @@ urlpatterns = [
 
     path('books/', book_list, name='book_list'),
 
-    path('home_librarian', HomeView.as_view(), name='home'),  # Главная страница
+    path('home_librarian', HomeView.as_view(), name='home'),
 ]
