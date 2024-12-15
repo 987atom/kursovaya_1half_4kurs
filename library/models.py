0bc3accec6d7d17b80d230f3ae9cb34140ea0b40
987atom <isip_m.k.davydov@mpt.ru>
2024-12-16 01:47:13 +0300

    5

--- library/models.py
+++ library/models.py
@@ -15,14 +15,17 @@ def assign_permissions():
     }
 
     for codename, name in permissions.items():
-        permission, created = Permission.objects.get_or_create(codename=codename, name=name)
+        permission, created = Permission.objects.get_or_create(
+            codename=codename, name=name)
         admin_group.permissions.add(permission)
         librarian_group.permissions.add(permission)
 
 class Author(models.Model):
     name = models.CharField(max_length=255, verbose_name=_("Имя"))
-    birth_date = models.DateField(null=True, blank=True, verbose_name=_("Дата рождения"))
-    nationality = models.CharField(max_length=100, null=True, blank=True, verbose_name=_("Национальность"))
+    birth_date = models.DateField(null=True, blank=True,
+                                  verbose_name=_("Дата рождения"))
+    nationality = models.CharField(max_length=100,
+                                   null=True, blank=True, verbose_name=_("Национальность"))
 
     def __str__(self):
         return self.name
@@ -45,9 +48,11 @@ class Genre(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255, verbose_name=_("Название"))
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name=_("Автор"))
+    author = models.ForeignKey(Author,
+                               on_delete=models.CASCADE, verbose_name=_("Автор"))
     publication_year = models.IntegerField(verbose_name=_("Год публикации"))
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, verbose_name=_("Жанр"))
+    genre = models.ForeignKey(Genre,
+                              on_delete=models.CASCADE, verbose_name=_("Жанр"))
 
     def __str__(self):
         return self.title
@@ -58,9 +63,11 @@ class Book(models.Model):
 
 
 class BookExemplar(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name=_("Книга"))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             verbose_name=_("Книга"))
     print_date = models.DateField(verbose_name=_("Дата печати"))
-    condition = models.CharField(max_length=100, verbose_name=_("Состояние"))
+    condition = models.CharField(max_length=100,
+                                 verbose_name=_("Состояние"))
 
     class Meta:
         unique_together = ('book', 'id')
@@ -72,7 +79,8 @@ class BookExemplar(models.Model):
 
 
 class Role(models.Model):
-    role_name = models.CharField(max_length=100, verbose_name=_("Название роли"))
+    role_name = models.CharField(max_length=100,
+                                 verbose_name=_("Название роли"))
 
     def __str__(self):
         return self.role_name
@@ -102,10 +110,14 @@ class Reader(models.Model):
     first_name = models.CharField(max_length=100, verbose_name=_("Имя"))
     last_name = models.CharField(max_length=100, verbose_name=_("Фамилия"))
     email = models.EmailField(unique=True, verbose_name=_("Электронная почта"))
-    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name=_("Телефон"))
-    username = models.CharField(max_length=100, unique=True, verbose_name=_("Имя пользователя"))
+    phone = models.CharField(max_length=15, null=True,
+                             blank=True, verbose_name=_("Телефон"))
+    username = models.CharField(max_length=100,
+                                unique=True,
+                                verbose_name=_("Имя пользователя"))
     password = models.CharField(max_length=255, verbose_name=_("Пароль"))
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name=_("Роль"))
+    role = models.ForeignKey(Role, on_delete=models.CASCADE,
+                             verbose_name=_("Роль"))
     last_login = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
@@ -122,9 +134,11 @@ class Staff(models.Model):
     first_name = models.CharField(max_length=100, verbose_name=_("Имя"))
     last_name = models.CharField(max_length=100, verbose_name=_("Фамилия"))
     position = models.CharField(max_length=100, verbose_name=_("Должность"))
-    username = models.CharField(max_length=100, unique=True, verbose_name=_("Имя пользователя"))
+    username = models.CharField(max_length=100, unique=True,
+                                verbose_name=_("Имя пользователя"))
     password = models.CharField(max_length=255, verbose_name=_("Пароль"))
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name=_("Роль"))
+    role = models.ForeignKey(Role, on_delete=models.CASCADE,
+                             verbose_name=_("Роль"))
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.position})"
@@ -135,7 +149,8 @@ class Staff(models.Model):
 
 
 class Shelf(models.Model):
-    location = models.CharField(max_length=255, verbose_name=_("Место нахождения"))
+    location = models.CharField(max_length=255,
+                                verbose_name=_("Место нахождения"))
     capacity = models.IntegerField(verbose_name=_("Вместимость"))
 
     def __str__(self):
@@ -147,11 +162,15 @@ class Shelf(models.Model):
 
 
 class Loan(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name=_("Книга"))
-    reader = models.ForeignKey(Reader, on_delete=models.CASCADE, verbose_name=_("Читатель"))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             verbose_name=_("Книга"))
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE,
+                               verbose_name=_("Читатель"))
     loan_date = models.DateField(verbose_name=_("Дата займа"))
-    return_date = models.DateField(null=True, blank=True, verbose_name=_("Дата возврата"))
-    is_returned = models.BooleanField(default=False, verbose_name=_("Возвращено?"))
+    return_date = models.DateField(null=True, blank=True,
+                                   verbose_name=_("Дата возврата"))
+    is_returned = models.BooleanField(default=False,
+                                      verbose_name=_("Возвращено?"))
 
     def __str__(self):
         return f"Заем {self.book.title} читателю {self.reader.username}"
@@ -162,8 +181,10 @@ class Loan(models.Model):
 
 
 class BooksShelves(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name=_("Книга"))
-    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, verbose_name=_("Полка"))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             verbose_name=_("Книга"))
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE,
+                              verbose_name=_("Полка"))
 
     def __str__(self):
         return f"{self.book.title} на полке {self.shelf.location}"
